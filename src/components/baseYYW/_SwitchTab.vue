acbc2ebcd7a5c9664a385549646ee0ae2629c6ed
dashboard小队 <879089262@qq.com>
2019-05-23 17:47:31 +0800

    +++

---
+++ src/components/baseYYW/_SwitchTab.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <ul class="switchTab">
+            <li 
+                :class="[switchIndex == index ? 'selected' : '']"
+                v-for="(item, index) of switchList"
+                :key="index"
+                @click="switchTab(index)"
+            >{{item.name}}</li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        switchIndex: Number,
+        switchList: Array
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        switchTab(index) {
+            this.$emit('switchTab', index)
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.switchTab {
+    display: flex;
+    flex: 1;
+    height: 30px;
+    justify-content: flex-start;
+    li {
+        display: inline-block;
+        // width:56px;
+        font-size:14px;
+        font-weight:400;
+        margin-right: 20px;
+    }
+    .selected {
+        color:#DDAF59;
+        position: relative;
+    }
+    .selected:after {
+        content: '';
+        position: absolute;
+        width: 20px;
+        height: 3px;
+        font-weight: 500;
+        left: 0;
+        right: 0;
+        margin: auto;
+        bottom: 5px;
+        background-color: #DDAF59;
+        border-radius: 2px;
+    }
+}
+</style>
+
---
+++ src/components/baseYYW/_TableTab.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="Table">
+        <ul>
+            <li 
+                v-for="(item, index) of tableGroup"
+                :key="index"
+                :class="[tableCurrent == index ? 'selected' : '']"
+                @click="tableChange(index)"
+            >{{item.name}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tableGroup: Array,
+        tableCurrent: Number
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        tableChange(index) {
+            this.$emit('tableChange', index)
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.Table {
+    width: 100%;
+    ul {
+        display: flex;
+        flex: 1;
+        line-height: 34px;
+        height: 34px;
+        justify-content: space-between;
+        li {
+            width: 33.3%;
+            text-align: center;
+            background: rgba(250,250,250,1);
+        }
+        .selected {
+            background: #fff;
+            height: 33px;
+            color: rgba(221,175,89,1);
+            position: relative;
+        }
+        .selected:after {
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 1px;
+            font-weight: 500;
+            left: 0;
+            right: 0;
+            margin: auto;
+            bottom: 0;
+            background-color: #DDAF59;
+            border-radius: 2px;
+        }
+    }
+}
+</style>
+
+
