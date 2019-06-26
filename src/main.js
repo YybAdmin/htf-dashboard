aea717087f3a97a0539d00db8c40b0c82abf5ac0
dashboard小队 <879089262@qq.com>
2019-06-26 14:13:07 +0800

    +++

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ Vue.prototype.$http = axios
 import 'vx-easyui/dist/themes/default/easyui.css'
 import 'vx-easyui/dist/themes/icon.css'
 import 'vx-easyui/dist/themes/vue.css'
+import '@/assets/css/easyui/myEasyui.css'
 import EasyUI from 'vx-easyui'
 Vue.use(EasyUI)
 
