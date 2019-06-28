45e9a54116d85224cab00592e8f2dc00e2b1342d
dashboard小队 <879089262@qq.com>
2019-06-28 17:25:03 +0800

    +++

--- src/components/cmpxCom/BaseDataGrid.vue
+++ src/components/cmpxCom/BaseDataGrid.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <DataGrid :data="data"
-              :frozenWidth="width">
+              :frozenWidth="width"
+              :rowCss="rowCss">
+      <GridColumn align="center" cellCss="datagrid-td-rownumber" width="30" :frozen="true">
+        <template slot="body" slot-scope="scope">
+          {{scope.rowIndex + 1}}
+        </template>
+      </GridColumn>
       <GridColumn field="NAME" title="产品名称" :frozen="true"></GridColumn>
-      <GridColumn field="VALUE1" title="保有量(亿元)" align="right" width="100" sortable
-                  formatter="stf"></GridColumn>
+      <GridColumn field="VALUE1" title="保有量(亿元)" align="right" width="100" sortable></GridColumn>
       <GridColumn field="VALUE2" title="份额(亿份)" align="right" width="100" sortable></GridColumn>
       <GridColumn field="VALUE3" title="净收入(万元)" align="right" width="100" sortable></GridColumn>
     </DataGrid>
@@ -29,6 +34,11 @@
           })
         } catch (e) {
           console.log(e)
+        }
+      },
+      rowCss: function (row) {
+        if(row.ID % 2 == 0){
+          return {'background':'#EEE'}
         }
       }
     },
