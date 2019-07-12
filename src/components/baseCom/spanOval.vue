92ef8f65371dfc987da3b2aa0f33556609df78e1
dashboard小队 <879089262@qq.com>
2019-07-12 10:40:27 +0800

    ---

---
+++ src/components/baseCom/spanOval.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="group">
+      <span :key="item.index"
+            v-for="(item,i) in list"
+            @click="changeTheme(i,'self')"
+            :class="[spanIndex == i ? 'selected' : '', spanIndex == i ?{themeGold: theme ,themeCoffee:!theme}:'']">
+        {{item.name}}
+      </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SpanSwiper',
+    props: {
+      itemList: {type: Array, default: []}, slidesPerView: {type: Number, default: 4}
+    },
+    data() {
+      return {
+        spanIndex: 0,
+        theme: this.$myUtil.theme,
+        list: this.itemList
+      }
+    },
+    methods: {
+      changeTheme(i, flag) {
+        this.spanIndex = i
+        if (flag === 'self') {
+          this.$emit('comChanged', i + 1)
+        }
+      }
+    }
+  }
+</script>
+<style scoped lang="less">
+  @gold: #ddaf59;
+  @goldBack: #fef7e9;
+  @coffee: #483c39;
+  @coffeeFont: #666;
+  @coffeeBack: #e9e2e0;
+  .group{
+    text-align: left;
+  }
+  .themeGold {
+    border-color: @gold;
+    color: @gold;
+    background-color: @goldBack;
+  }
+
+  .themeCoffee {
+    border-color: @coffee;
+    color: @coffee;
+    background-color: @coffeeBack;
+  }
+
+  span {
+    background-color: white;
+    color: #999999;
+    font-size: 12px;
+    padding: 2px 11px;
+    margin: auto;
+    border: 1px solid white;
+    margin-left:10px;
+  }
+
+  .selected {
+    border-radius: 10px;
+    outline: none;
+  }
+</style>
--- src/report/yidong.vue
+++ src/report/yidong.vue
@@ -1,60 +1,62 @@
 <template>
-  <div>
+  <div style="overflow-x: hidden;">
     <title>渠道异动监控</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <div style="background: white;width: 100%;">
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <div style="background: white;">
       <table style="width: 100%;line-height: 44px;">
         <tr>
           <td>
             <search :searchInfo="comName.searchInfo"></search>
           </td>
-          <td style="color: #999999;font-size: 13px;text-align:right;width: 112px;" @click="choseLv($event,0)">异动敏感度<span id="lv" class="lvsty">高</span></td>
+          <td style="color: #999999;font-size: 13px;text-align:right;width: 112px;" @click="choseLv($event,0)">
+            异动敏感度<span id="lv" class="lvsty">高</span></td>
           <td><span class="spsty"></span></td>
         </tr>
       </table>
     </div>
-    <div style="margin-bottom: 10px;">
-      <table style="width: 100%;table-layout: fixed">
+    <table style="width: 100%;table-layout: fixed;margin-bottom: 10px;">
+      <tr>
+        <td class="DimTitle">异动频度:</td>
+        <td>
+          <Dim ref="ydPinDu" @comChanged="changePageState($event,'f_pindu')"
+               :itemList="comName.pinDu" :slidesPerView="4.6"></Dim>
+        </td>
+      </tr>
+      <tr>
+        <td class="DimTitle">产品类型:</td>
+        <td>
+          <Dim ref="proType" @comChanged="changePageState($event,'f_protype')" :itemList="comName.proType"
+               :slidesPerView="5"></Dim>
+        </td>
+      </tr>
+      <tr>
+        <td class="DimTitle">平&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;台:</td>
+        <td>
+          <Dim ref="platform" @comChanged="changePageState($event,'f_platform')" :itemList="comName.platform"
+               :slidesPerView="5"></Dim>
+        </td>
+      </tr>
+      <tr>
+        <td class="DimTitle">指&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;标:</td>
+        <td>
+          <Dim ref="target" @comChanged="changePageState($event,'f_target')" :itemList="comName.target"
+               :slidesPerView="5"></Dim>
+        </td>
+      </tr>
+    </table>
+    <div id="lvTab" class="lvDivsty" style="display: none">
+      <table class="tabsty">
         <tr>
-          <td style="width: 82px;text-align: right;font-weight: 400">异动频度:</td>
-          <td>
-            <div>
-              <swiperOvalNoAll style="margin-left: -25px;" ref="ydPinDu" @comChanged="changePageState($event,'f_pindu')" v-bind:itemList="comName.pinDu"></swiperOvalNoAll>
-            </div>
-          </td>
+          <td @click="choseLv($event,1)">高</td>
         </tr>
         <tr>
-          <td style="width: 82px;text-align: right;font-weight: 400">产品类型:</td>
-          <td>
-            <div>
-              <swiperOval ref="proType" @comChanged="changePageState($event,'f_protype')" v-bind:itemList="comName.proType" v-bind:slidesPerView="4"></swiperOval>
-            </div>
-          </td>
+          <td @click="choseLv($event,2)">中</td>
         </tr>
         <tr>
-          <td style="width: 82px;text-align: right;font-weight: 400">平&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;台:</td>
-          <td>
-            <div>
-              <swiperOval ref="platform" @comChanged="changePageState($event,'f_platform')" v-bind:itemList="comName.platform" v-bind:slidesPerView="3"></swiperOval>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td style="width: 82px;text-align: right;font-weight: 400">指&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;标:</td>
-          <td>
-            <div>
-              <swiperOval ref="target" @comChanged="changePageState($event,'f_target')" v-bind:itemList="comName.target"></swiperOval>
-            </div>
-          </td>
+          <td @click="choseLv($event,3)">低</td>
         </tr>
       </table>
-      <div id="lvTab" class="lvDivsty" style="display: none">
-        <table class="tabsty">
-          <tr><td @click="choseLv($event,1)">高</td></tr>
-          <tr><td @click="choseLv($event,2)">中</td></tr>
-          <tr><td @click="choseLv($event,3)">低</td></tr>
-        </table>
-      </div>
     </div>
     <divSplit></divSplit>
     <div>
@@ -67,116 +69,124 @@
 </template>
 
 <script>
-import $ from 'jquery'
-import swiperOval from '@/components/baseCom/swiperOval'
-import swiperOvalNoAll from '@/components/baseCom/swiperOvalNoAll'
-import divSplit from '@/components/baseCom/DivSplit'
-import ydsummary from '@/components/yidong/dataSummary'
-import datacard from '@/components/yidong/dataCard'
-import search from '@/components/baseCom/InputSearch'
-export default {
-  name: "yidong",
-  components: {search,swiperOval,swiperOvalNoAll,divSplit,ydsummary,datacard},
-  data(){
+  import $ from 'jquery'
+  import swiperOval from '@/components/baseCom/swiperOval'
+  import Dim from '@/components/baseCom/spanOval'
+  import divSplit from '@/components/baseCom/DivSplit'
+  import ydsummary from '@/components/yidong/dataSummary'
+  import datacard from '@/components/yidong/dataCard'
+  import search from '@/components/baseCom/InputSearch'
+
+  export default {
+    name: "yidong",
+    components: {search, Dim, divSplit, ydsummary, datacard},
+    data() {
       return {
-        comName:{
-          searchInfo:{placeholder:"渠道名称/产品名称/产品代码",pagePath:'/jingpin',ifGoOtherPage:true},
-          pinDu:[{name:'当日'},{name:'连续两日'},{name:'连续三日'}],
-          proType:[{name:'股票型'},{name:'货币型'}],
-          platform:[{name:'自有平台'},{name:'三方三大平台'}],
-          target:[{name:'份额'},{name:'金额'},{name:'笔数'}]
+        comName: {
+          searchInfo: {placeholder: "渠道名称/产品名称/产品代码", pagePath: '/jingpin', ifGoOtherPage: true},
+          pinDu: [{name: '当日'}, {name: '连续两日'}, {name: '连续三日'}],
+          proType: [{name: '全部'}, {name: '股票'}, {name: '货币'}],
+          platform: [{name: '全部'}, {name: '自有平台'}, {name: '三方三大平台'}],
+          target: [{name: '全部'}, {name: '份额'}, {name: '金额'}, {name: '笔数'}]
         },
-        pageVal:{
-          ydPinDu:1,
-          proType:1,
-          platform:1,
-          target:1
+        pageVal: {
+          ydPinDu: 1,
+          proType: 1,
+          platform: 1,
+          target: 1
         },
-        test:{
-          sum:[{VALUE1:'3',VALUE2:'5',VALUE3:'50',VALUE4:'900'}]
+        test: {
+          sum: [{VALUE1: '3', VALUE2: '5', VALUE3: '50', VALUE4: '900'}]
         }
       }
-  },
-  mounted () {
-
-  },
-  methods:{
-    ydGetDataAndDraw:function(pageVal,reflashFlag){
-      if (flag === '') {
-
-      }else if (flag === 'no') {
-        return false
-      }
     },
-    changePageState:function (val, flag) {
-      let reflashFlag = 'no'
-      // 异动频度
-      if(flag == 'f_pindu'){
-        reflashFlag = this.changePinDu(val)
-      }
-      // 产品类型
-      if(flag == 'f_protype'){
-        reflashFlag = this.changeProType(val)
-      }
-      // 平台
-      if(flag == 'f_platform'){
-        reflashFlag = this.changePlatform(val)
-      }
-      // 平台
-      if(flag == 'f_target'){
-        reflashFlag = this.changeTarget(val)
-      }
-      console.log(this.pageVal)
-      this.ydGetDataAndDraw(this.pageVal, reflashFlag)
-    },
-    choseLv:function (e,i) {
-      if(i==0){
-        $("#lvTab").show()
-      }else if(i==1){
-        $("#lvTab").hide()
-        $("#lv").text(e.currentTarget.innerHTML)
-        $("#lv").css('color','#F4333C')
-      }else if(i==2){
-        $("#lvTab").hide()
-        $("#lv").text(e.currentTarget.innerHTML)
-        $("#lv").css('color','#FBC25C')
-      }else if(i==3){
-        $("#lvTab").hide()
-        $("#lv").text(e.currentTarget.innerHTML)
-        $("#lv").css('color','#88D2FF')
-      }
-    },
-    changePinDu:function (val) {
-      this.pageVal.ydPinDu = val
+    mounted() {
 
-      return 'pinDu'
     },
-    changeProType:function (val) {
-      this.pageVal.proType = val
+    methods: {
+      ydGetDataAndDraw: function (pageVal, reflashFlag) {
+        if (flag === '') {
 
-      return 'proType'
-    },
-    changePlatform:function (val) {
-      this.pageVal.platform = val
+        } else if (flag === 'no') {
+          return false
+        }
+      },
+      changePageState: function (val, flag) {
+        let reflashFlag = 'no'
+        // 异动频度
+        if (flag == 'f_pindu') {
+          reflashFlag = this.changePinDu(val)
+        }
+        // 产品类型
+        if (flag == 'f_protype') {
+          reflashFlag = this.changeProType(val)
+        }
+        // 平台
+        if (flag == 'f_platform') {
+          reflashFlag = this.changePlatform(val)
+        }
+        // 平台
+        if (flag == 'f_target') {
+          reflashFlag = this.changeTarget(val)
+        }
+        console.log(this.pageVal)
+        this.ydGetDataAndDraw(this.pageVal, reflashFlag)
+      },
+      choseLv: function (e, i) {
+        if (i == 0) {
+          $("#lvTab").show()
+        } else if (i == 1) {
+          $("#lvTab").hide()
+          $("#lv").text(e.currentTarget.innerHTML)
+          $("#lv").css('color', '#F4333C')
+        } else if (i == 2) {
+          $("#lvTab").hide()
+          $("#lv").text(e.currentTarget.innerHTML)
+          $("#lv").css('color', '#FBC25C')
+        } else if (i == 3) {
+          $("#lvTab").hide()
+          $("#lv").text(e.currentTarget.innerHTML)
+          $("#lv").css('color', '#88D2FF')
+        }
+      },
+      changePinDu: function (val) {
+        this.pageVal.ydPinDu = val
 
-      return 'platform'
-    },
-    changeTarget:function (val) {
-      this.pageVal.platform = val
+        return 'pinDu'
+      },
+      changeProType: function (val) {
+        this.pageVal.proType = val
 
-      return 'target'
+        return 'proType'
+      },
+      changePlatform: function (val) {
+        this.pageVal.platform = val
+
+        return 'platform'
+      },
+      changeTarget: function (val) {
+        this.pageVal.platform = val
+
+        return 'target'
+      }
     }
   }
-}
 </script>
 
 <style scoped lang="less">
-  .lvsty{
+  .DimTitle {
+    width: 82px;
+    text-align: right;
+    font-weight: 400;
+  }
+
+  .lvsty {
     color: #F4333C;
     font-size: 13px;
     margin-left: 7px;
   }
-  .spsty{
+
+  .spsty {
     width: 0;
     height: 0;
     border-bottom: 4px solid #999999;
@@ -185,15 +195,17 @@ export default {
     display: block;
     margin-right: 5px;
   }
-  .lvDivsty{
-    position:fixed;
+
+  .lvDivsty {
+    position: fixed;
     right: 10px;
     width: 71px;
     height: 126px;
-    top:5%;
+    top: 5%;
     z-index: 10
   }
-  .tabsty{
+
+  .tabsty {
     width: 71px;
     height: 126px;
     border: 1px solid #FFFFFF;
@@ -201,19 +213,22 @@ export default {
     border-radius: 5px;
     background-color: white
   }
-  .tabsty tr:first-child td{
+
+  .tabsty tr:first-child td {
     font-size: 15px;
     color: #F4333C;
     font-weight: bold;
     border-bottom: 1px solid #EEEEEE;
   }
-  .tabsty tr:nth-child(2) td{
+
+  .tabsty tr:nth-child(2) td {
     font-size: 15px;
     color: #FBC25C;
     font-weight: bold;
     border-bottom: 1px solid #EEEEEE;
   }
-  .tabsty tr:nth-child(3) td{
+
+  .tabsty tr:nth-child(3) td {
     font-size: 15px;
     color: #88D2FF;
     font-weight: bold
