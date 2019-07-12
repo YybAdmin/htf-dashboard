8c5a3c673bc0cd46cf471566c4cfd8dd51e488e3
dashboard小队 <879089262@qq.com>
2019-07-12 14:02:25 +0800

    ---

--- src/components/baseCom/selectDwn.vue
+++ src/components/baseCom/selectDwn.vue
@@ -2,49 +2,41 @@
   <div class="group">
     <div class="lvhead">
       <table>
-        <tr @click="choseLv($event,0)">
-          <td style="color: #999999;font-size: 13px;text-align:right;width: 112px;">异动敏感度</td>
-          <td><span id="lv" class="lvsty">高</span></td>
+        <tr @click="toggleDiv()">
+          <td id="td1">异动敏感度</td>
+          <td id="lv" class="lvsty">高</td>
           <td><span class="spsty"></span></td>
         </tr>
       </table>
     </div>
-    <div id="lvTab" class="lvDivsty" style="display: none">
-      <table class="tabsty">
-        <tr>
-          <td @click="choseLv($event,1)">高</td>
-        </tr>
-        <tr>
-          <td @click="choseLv($event,2)">中</td>
-        </tr>
-        <tr>
-          <td @click="choseLv($event,3)">低</td>
-        </tr>
-      </table>
+    <div id="lvTab" style="display: none">
+      <div id="op">
+        <div id="opc1" class="opc" @click="choseLv($event,1)">高</div>
+        <div id="opc2" class="opc" @click="choseLv($event,2)">中</div>
+        <div id="opc3" class="opc" @click="choseLv($event,3)">低</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import $ from 'jquery'
+
   export default {
     name: "selectDwn",
+    props: {},
     methods: {
-      choseLv: function (e,i) {
-        if (i == 0) {
-          $("#lvTab").show()
-        } else if (i == 1) {
-          $("#lvTab").hide()
-          $("#lv").text(e.currentTarget.innerHTML)
-          $("#lv").css('color', '#F4333C')
+      toggleDiv: function () {
+        $("#lvTab").toggle()
+      },
+      choseLv: function (e, i) {
+        $("#lvTab").hide()
+        if (i == 1) {
+          $("#lv").text("高").css('color', '#F4333C')
         } else if (i == 2) {
-          $("#lvTab").hide()
-          $("#lv").text(e.currentTarget.innerHTML)
-          $("#lv").css('color', '#FBC25C')
+          $("#lv").text("中").css('color', '#FBC25C')
         } else if (i == 3) {
-          $("#lvTab").hide()
-          $("#lv").text(e.currentTarget.innerHTML)
-          $("#lv").css('color', '#88D2FF')
+          $("#lv").text("低").css('color', '#88D2FF')
         }
         this.$emit('comChanged', i + 1)
       }
@@ -53,37 +45,63 @@
 </script>
 
 <style scoped lang="less">
-  .lvsty {
-    color: #F4333C;
-    font-size: 13px;
-    margin-left: 7px;
+  .group {
+    .lvhead {
+      #td1 {
+        color: #999999;
+        font-size: 13px;
+        text-align: right;
+        width: 112px;
+      }
+
+      .lvsty {
+        color: #F4333C;
+        font-size: 13px;
+        margin-left: 7px;
+      }
+
+      .spsty {
+        width: 0;
+        height: 0;
+        border-bottom: 4px solid #999999;
+        border-left: 4px solid transparent;
+        margin-top: 10px;
+        display: block;
+        margin-right: 5px;
+      }
+    }
+
+    #lvTab {
+      position: fixed;
+      right: 10px;
+      width: 71px;
+      height: 126px;
+      line-height: 43px;
+      top: 5%;
+      z-index: 10;
+      border: 1px solid #FFFFFF;
+      box-shadow: #DDDDDD 2px 2px 2px 2px;
+      border-radius: 5px;
+      background-color: white;
+
+      #op {
+        .opc {
+          border-bottom: 1px solid #EEE;
+        }
+
+        #opc1 {
+          color: #F4333C;
+        }
+
+        #opc2 {
+          color: #FBC25C;
+        }
+
+        #opc3 {
+          color: #88D2FF;
+        }
+      }
+    }
   }
 
-  .spsty {
-    width: 0;
-    height: 0;
-    border-bottom: 4px solid #999999;
-    border-left: 4px solid transparent;
-    margin-top: 10px;
-    display: block;
-    margin-right: 5px;
-  }
-
-  .lvDivsty {
-    position: fixed;
-    right: 10px;
-    width: 71px;
-    height: 126px;
-    top: 5%;
-    z-index: 10
-  }
-
-  .tabsty {
-    width: 71px;
-    height: 126px;
-    border: 1px solid #FFFFFF;
-    box-shadow: #DDDDDD 2px 2px 2px 2px;
-    border-radius: 5px;
-    background-color: white
-  }
 </style>
