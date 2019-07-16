8afc13487fb93f22b389542a17a11d8661b284b3
dashboard小队 <879089262@qq.com>
2019-07-16 09:29:18 +0800

    ---

--- src/apiList.js
+++ src/apiList.js
@@ -1,4 +1,4 @@
-let state = 1
+let state = 0
 let hrefArr = [
   '',
   'https://iapp.htffund.com/microservice/iapp-dashboard/api',
@@ -9,26 +9,30 @@ let href = hrefArr[state] + '/report'
 let href2 = hrefArr[state] + '/dev'
 
 let API_LIST = {
-   state:state,
-  'hujinDataDate': href2 + '/hujin/dataDate',
-  'hujinHeadData': href2 + '/hujin/headData',
-  'hujinChartUp': href2 + '/hujin/chartUp',
-  'hujinChartMid': href2 + '/hujin/chartMid',
-  'hujinChartDwn': href2 + '/hujin/chartDwn',
-  'hujinKehuHeadData': href2 + '/hujin/hujinKehuHeadData',
-  'hujinPdf': href + '/pdf/hujinPdf',
-  'hujinKehuChart': href2 + '/hujin/kehuChart',
-
-
-  'dataGridDemo': href + '/dataGrid/getData',
-  'treeGridDemo': href + '/treeGrid/getData',
-
-  'yidongSummary':href + '/yidong/summary',
-  'yidongDataCard':href + '/yidong/dataCard',
-  'yidongHead':href + '/yidong/headData',
-  'yidongChart':href + '/yidong/chartData',
-  'yidongGrid':href + '/yidong/chartGrid',
-
-  'allUserPro': href + '/selPro/getUserPro'
+  state: state,
+  huijin: {
+    'hujinDataDate': href + '/hujin/dataDate',
+    'hujinHeadData': href + '/hujin/headData',
+    'hujinChartUp': href + '/hujin/chartUp',
+    'hujinChartMid': href + '/hujin/chartMid',
+    'hujinChartDwn': href + '/hujin/chartDwn',
+    'hujinKehuHeadData': href + '/hujin/hujinKehuHeadData',
+    'hujinPdf': href + '/pdf/hujinPdf',
+    'hujinKehuChart': href + '/hujin/kehuChart',
+  },
+  dataGrid: {
+    'dataGridDemo': href + '/dataGrid/getData',
+    'treeGridDemo': href + '/treeGrid/getData',
+  },
+  yidong: {
+    'yidongSummary': href + '/yidong/summary',
+    'yidongDataCard': href + '/yidong/dataCard',
+    'yidongHead': href + '/yidong/headData',
+    'yidongChart': href + '/yidong/chartData',
+    'yidongGrid': href + '/yidong/chartGrid',
+  },
+  selfPro: {
+    'allUserPro': href + '/selPro/getUserPro'
+  }
 }
 export default API_LIST
