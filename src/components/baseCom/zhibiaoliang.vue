f9e47800646d215913d152961dd7795089c2dde2
dashboard小队 <879089262@qq.com>
2019-07-22 18:12:39 +0800

    ---

--- src/components/baseCom/zhibiaoliang.vue
+++ src/components/baseCom/zhibiaoliang.vue
@@ -2,27 +2,27 @@
     <div class="container">
       <div>
         <table>
-          <tr>
-            <td><img src="@/assets/img/zhibiao/zhibiao1.png" class="imgs"></td>
-            <td><img src="@/assets/img/zhibiao/zhibiao2.png" class="imgs"></td>
-            <td><img src="@/assets/img/zhibiao/zhibiao3.png" class="imgs"></td>
+          <tr class="imgtd">
+            <td><img src="@/assets/img/zhibiao/zhibiao1.png"></td>
+            <td><img src="@/assets/img/zhibiao/zhibiao2.png"></td>
+            <td><img src="@/assets/img/zhibiao/zhibiao3.png"></td>
           </tr>
-          <tr>
-            <td class="text-aqua">365</td>
-            <td class="text-aqua">80</td>
-            <td class="text-aqua">35</td>
+          <tr class="text-aqua">
+            <td>365</td>
+            <td>80</td>
+            <td>35</td>
           </tr>
-          <tr>
-            <td class="text-14">指标总数</td>
-            <td class="text-14">未确认指标总数</td>
-            <td class="text-14">未更新指标数</td>
+          <tr class="text-14">
+            <td>指标总数</td>
+            <td>未确认指标总数</td>
+            <td>未更新指标数</td>
           </tr>
-          <tr>
+          <tr class="text-12">
             <td>
             <Help></Help>
             </td>
-            <td class="text-12">未确认比率:15%</td>
-            <td class="text-12">未确认比率:15%</td>
+            <td><span>未确认比率:15%</span></td>
+            <td><span>未确认比率:15%</span></td>
           </tr>
         </table>
       </div>
@@ -41,34 +41,42 @@
     }
 </script>
 <style scoped lang="less">
-
-  table {
-    width: 100%;
-    table-layout: fixed;
-    border-collapse: collapse;
-
-  td {
-    font-weight: 400;
-    line-height: 34px;
-    text-align: center;
-  }
-  }
-  .imgs {
-    margin-top: 12px;
-  }
-  .text-aqua {
-    font-size: 20px;
-    color: #333333;
-  }
-  .text-14 {
-    font-size: 14px;
-    color: #333333;
-
-  }
-  .text-12 {
-    font-size: 12px;
-    color: #DDAF59;
-    margin-bottom: 15px;
+  .container{
+    table {
+      width: 100%;
+      table-layout: fixed;
+      border-collapse: collapse;
+      margin-top:8px;
+      text-align: center;
+      .imgtd{
+        line-height:36px;
+        img{
+          vertical-align: middle;
+        }
+      }
+      .text-aqua {
+        font-size: 20px;
+        line-height:28px;
+        color: #333333;
+        font-weight: 600;
+      }
+      .text-14 {
+        font-size: 14px;
+        color: #333333;
+        line-height:20px;
+      }
+      .text-12 {
+        font-size: 12px;
+        line-height:32px;
+        color: #DDAF59;
+        margin-bottom: 9px;
+        span{
+          background-color: rgba(249,242,229,1);
+          padding:3px 7px;
+          border-radius: 10px;
+        }
+      }
+    }
   }
 
 </style>
