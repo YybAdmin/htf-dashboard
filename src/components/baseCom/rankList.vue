93494685d14b7d4367985df879809289b91536f4
dashboard小队 <879089262@qq.com>
2019-07-22 13:06:23 +0800

    ---

---
+++ src/components/baseCom/rankList.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="group">
+    <div class="content">
+      <div class="title">
+        <span class="leaf1" style="display:inline-flex;"></span>
+        <span class="leaf2" style="display:inline-flex;"></span>
+        <span style="display:inline-flex;">{{this.title}}</span>
+        <span class="leaf3" style="display:inline-flex;"></span>
+        <span class="leaf4" style="display:inline-flex;"></span>
+      </div>
+      <table>
+        <tr v-for="(item,i) in itemList"
+            :key="i">
+          <td v-if="i===0"><img src="@/assets/img/yjy/rank1.svg"></td>
+          <td v-if="i===1"><img src="@/assets/img/yjy/rank2.svg"></td>
+          <td v-if="i===2"><img src="@/assets/img/yjy/rank3.svg"></td>
+          <td v-if="i>=3">{{i+1}}</td>
+          <td><img style="height:40px;width:40px;border-radius: 20px;" src="@/assets/img/yjy/tx.jpg"></td>
+          <td style="text-align: left;">{{item.NAME}}</td>
+          <td style="width:50%;text-align: right;">{{item.VALUE}}</td>
+        </tr>
+      </table>
+      <div class="footer">更多></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "rankList",
+    props: ["title"],
+    data() {
+      return {
+        itemList: [
+          {RANK: '1', PHOTO: '', NAME: '蔡志文', VALUE: '888'},
+          {RANK: '2', PHOTO: '', NAME: '卞正', VALUE: '777'},
+          {RANK: '3', PHOTO: '', NAME: '陈潇杨', VALUE: '666'},
+          {RANK: '4', PHOTO: '', NAME: '崔文琦', VALUE: '555'},
+          {RANK: '5', PHOTO: '', NAME: '董超', VALUE: '445'},
+          {RANK: '6', PHOTO: '', NAME: '付鹏飞', VALUE: '444'},
+          {RANK: '7', PHOTO: '', NAME: '韩超', VALUE: '333'},
+          {RANK: '8', PHOTO: '', NAME: '何往', VALUE: '323'},
+          {RANK: '9', PHOTO: '', NAME: '黄耀锋', VALUE: '322'},
+          {RANK: '10', PHOTO: '', NAME: '劳杰男', VALUE: '222'}]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  .group {
+    background-color: #F6F6F6;
+    padding: 15px;
+
+    .content {
+      background-color: #FFFFFF;
+      padding: 0px 15px;
+
+      .leaf1 {
+        border: 7px solid #E8C990;
+        border-radius: 0px 12px;
+        transform: rotate(-10deg);
+        margin-bottom: -5px;
+      }
+
+      .leaf2 {
+        border: 7px solid #ddaf59;
+        border-radius: 0px 12px;
+        transform: rotate(30deg);
+        margin-left: -14px;
+      }
+
+      .leaf3 {
+        border: 7px solid #ddaf59;
+        border-radius: 0px 12px;
+        transform: rotate(70deg);
+        margin-right: -14px;
+      }
+
+      .leaf4 {
+        border: 7px solid #E8C990;
+        border-radius: 0px 12px;
+        transform: rotate(120deg);
+        margin-bottom: -5px;
+      }
+
+      .title {
+        font-size: 17px;
+        color: #DDAF59;
+        padding-top: 20px;
+        margin-bottom: 20px;
+      }
+
+      table {
+        width: 100%;
+        font-size: 16px;
+        color: #000;
+        line-height: 51px;
+        border-spacing: inherit;
+        border-collapse: collapse;
+        padding: 0px;
+
+        tr {
+          td {
+            border-bottom: 1px solid #eee;
+
+            img {
+              vertical-align: middle;
+            }
+          }
+        }
+      }
+      .footer{
+        font-size: 14px;
+        color: #ddaf59;
+        line-height:43px;
+      }
+    }
+  }
+</style>
--- src/report/chanpin/qudao.vue
+++ src/report/chanpin/qudao.vue
@@ -3,33 +3,58 @@
     <table style="padding-left:10px;word-break: keep-all;">
       <tr>
         <td>周期:</td>
-        <td><selDim :itemList="comName.dateList" :slidesPerView="5" @comChanged="changePageState($event,'f_dateType')"> </selDim></td>
+        <td>
+          <selDim :itemList="comName.dateList" :slidesPerView="5"
+                  @comChanged="changePageState($event,'f_dateType')"></selDim>
+        </td>
       </tr>
       <tr>
         <td>渠道:</td>
-        <td><selDim :itemList="comName.channelList" :slidesPerView="5" @comChanged="changePageState($event,'f_channel')"> </selDim></td>
+        <td>
+          <selDim :itemList="comName.channelList" :slidesPerView="5"
+                  @comChanged="changePageState($event,'f_channel')"></selDim>
+        </td>
       </tr>
       <tr>
         <td>排序:</td>
-        <td><selDim :itemList="comName.orderList" :slidesPerView="5" @comChanged="changePageState($event,'f_order')"> </selDim></td>
+        <td>
+          <selDim :itemList="comName.orderList" :slidesPerView="5"
+                  @comChanged="changePageState($event,'f_order')"></selDim>
+        </td>
       </tr>
       <tr>
         <td>自选:</td>
-        <td><selDim v-bind:itemList="comName.productList" @comChanged="changePageState($event,'f_product')"></selDim></td>
+        <td>
+          <selDim v-bind:itemList="comName.productList" @comChanged="changePageState($event,'f_product')"></selDim>
+        </td>
       </tr>
     </table>
     <LineSplit></LineSplit>
     <table style="table-layout: fixed;width: 100%;">
       <tr id="orderG1">
-        <td><BtnOrder ref="order1" :item="comName.orderItem[0]" @comChanged="changePageState($event,'f_orderChild')"></BtnOrder></td>
-        <td><BtnOrder ref="order2" :item="comName.orderItem[1]" @comChanged="changePageState($event,'f_orderChild')"></BtnOrder></td>
+        <td>
+          <BtnOrder ref="order1" :item="comName.orderItem[0]"
+                    @comChanged="changePageState($event,'f_orderChild')"></BtnOrder>
+        </td>
+        <td>
+          <BtnOrder ref="order2" :item="comName.orderItem[1]"
+                    @comChanged="changePageState($event,'f_orderChild')"></BtnOrder>
+        </td>
       </tr>
       <tr id="orderG2" style="display: none;">
-        <td><BtnOrder ref="order3" :item="comName.orderItem[2]" @comChanged="changePageState($event,'f_orderChild')"></BtnOrder></td>
-        <td><BtnOrder ref="order4" :item="comName.orderItem[3]" @comChanged="changePageState($event,'f_orderChild')"></BtnOrder></td>
+        <td>
+          <BtnOrder ref="order3" :item="comName.orderItem[2]"
+                    @comChanged="changePageState($event,'f_orderChild')"></BtnOrder>
+        </td>
+        <td>
+          <BtnOrder ref="order4" :item="comName.orderItem[3]"
+                    @comChanged="changePageState($event,'f_orderChild')"></BtnOrder>
+        </td>
       </tr>
     </table>
     <DivSplit></DivSplit>
+    <span>哈哈哈</span>
+    <ranklist :title="comName.rankTitle"></ranklist>
   </div>
 </template>
 
@@ -39,52 +64,57 @@
   import LineSplit from '@/components/baseCom/LineSplit'
   import selDim from '@/components/baseCom/swiperOvalNoAll'
   import BtnOrder from '@/components/baseCom/BtnOrder2'
+  import ranklist from '@/components/baseCom/rankList'
+
   export default {
     name: "qudao",
-    components:{DivSplit,LineSplit,selDim,BtnOrder},
+    components: {DivSplit, LineSplit, selDim, BtnOrder, ranklist},
     data() {
       return {
-        comName:{
-          dateList:[{name: '每日'}, {name: '每周'}, {name: '月度'}, {name: '季度'}, {name: '年度'}],
-          channelList:[{name: '全部'}, {name: '自有'}, {name: '三方'}],
-          orderList:[{name: '规模'}, {name: '资金'}],
-          orderItem:[{name:'保有量',value:1},{name:'份额',value:2},{name:'申购',value:3},{name:'净收入',value:4}],
-          productList: [{name: '全部'}, {name: '重点产品'}, {name: '自选产品>'}]
+        comName: {
+          dateList: [{name: '每日'}, {name: '每周'}, {name: '月度'}, {name: '季度'}, {name: '年度'}],
+          channelList: [{name: '全部'}, {name: '自有'}, {name: '三方'}],
+          orderList: [{name: '规模'}, {name: '资金'}],
+          orderItem: [{name: '保有量', value: 1}, {name: '份额', value: 2}, {name: '申购', value: 3}, {name: '净收入', value: 4}],
+          productList: [{name: '全部'}, {name: '重点产品'}, {name: '自选产品>'}],
+          rankTitle: '点评量排名'
         },
-        switch:{
-          orderGroup:true
+        switch: {
+          orderGroup: true
         }
       }
     },
-    methods:{
-      changePageState:function (i,flag) {
-        if(flag === 'f_dateType'){}
-        if(flag === 'f_channel'){}
-        if(flag === 'f_order'){
-            if(i===1){
-              $("#orderG1").show()
-              $("#orderG2").hide()
-              this.$refs.order1.init()
-              this.$refs.order2.reSet()
-            }else{
-              $("#orderG2").show()
-              $("#orderG1").hide()
-              this.$refs.order3.init()
-              this.$refs.order4.reSet()
-            }
+    methods: {
+      changePageState: function (i, flag) {
+        if (flag === 'f_dateType') {
         }
-        if(flag === 'f_orderChild'){
-          if(i===1){
+        if (flag === 'f_channel') {
+        }
+        if (flag === 'f_order') {
+          if (i === 1) {
+            $("#orderG1").show()
+            $("#orderG2").hide()
+            this.$refs.order1.init()
             this.$refs.order2.reSet()
-          }else if(i===2){
-            this.$refs.order1.reSet()
-          }else if(i===3){
+          } else {
+            $("#orderG2").show()
+            $("#orderG1").hide()
+            this.$refs.order3.init()
             this.$refs.order4.reSet()
-          }else if(i===4){
+          }
+        }
+        if (flag === 'f_orderChild') {
+          if (i === 1) {
+            this.$refs.order2.reSet()
+          } else if (i === 2) {
+            this.$refs.order1.reSet()
+          } else if (i === 3) {
+            this.$refs.order4.reSet()
+          } else if (i === 4) {
             this.$refs.order3.reSet()
           }
         }
-        if(flag === 'f_product'){
+        if (flag === 'f_product') {
           this.$router.push('/search')
         }
       }
