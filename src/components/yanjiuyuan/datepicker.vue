ec7c3dcad97f10a0c85a594551a0256ee2282459
dashboard小队 <879089262@qq.com>
2019-07-30 13:27:32 +0800

    +++

--- src/components/yanjiuyuan/datepicker.vue
+++ src/components/yanjiuyuan/datepicker.vue
@@ -1,19 +1,14 @@
 <template>
-
-  <div class="mui-content">
+  <div style="overflow-y: hidden;">
     <div class="mui-content-padded">
       <div class="ui-alert">自定义筛选时间</div>
-      <table style="width: 100%;table-layout: auto;line-height: 44px;">
-        <tr>
-          <td style="width: 54px;"><img src="@/assets/img/yjy/pie.png"></td>
-        </tr>
-      </table>
-      <div id='cityResult3' class="ui-alert">
-        <button id='showCityPicker3' class="mui-btn mui-btn-block" type='button'>开始时间</button>
+      <img src="@/assets/img/yjy/pie.png">
+      <div id='startDateBtn' class="ui-alert">
+        <button id='startDate' class="mui-btn mui-btn-block" type='button'>开始时间</button>
       </div>
       <div><img src="@/assets/img/yjy/dateto.png"></div>
-      <div id='cityResult' class="ui-alert">
-        <button id='showCityPicker' class="mui-btn mui-btn-block" type='button'>结束时间</button>
+      <div id='endDateBtn' class="ui-alert">
+        <button id='endDate' class="mui-btn mui-btn-block" type='button'>结束时间</button>
       </div>
       <div class="mui-btn-div">
         <button id='sumbit' class="mui-btn mui-btn-block" type='button'>完成</button>
@@ -28,76 +23,73 @@
   import 'vue-awesome-mui/mui/plugin/picker/css/mui.poppicker.css'
   import 'vue-awesome-mui/mui/plugin/picker/js/mui.picker'
   import 'vue-awesome-mui/mui/plugin/picker/js/mui.poppicker'
+
   export default {
     name: "datepicker",
     mounted() {
+      let _this = this
       let picker = new window.mui.PopPicker({
         layer: 3,
         buttons: ['cancle', 'ok']
       });
-      let _getParam = function(obj, param) {
+      let _getParam = function (obj, param) {
         return obj[param] || '';
       };
       //年月日
       let yearArray = new Array();
-      for (let i = 0; i < 25; i ++) {
+      for (let i = 0; i < 25; i++) {
         let monthArray = new Array();
-        for (let j = 0; j < 12; j ++) {
+        for (let j = 0; j < 12; j++) {
           //月的最后一天
           let year = i + 2016;
           let month = j;
-          let lastDay = new Date(year, month + 1, 0).getDate();//month 要加1,原本month是从0开始的,但是因为0是这个月的上个月,所以到了上个月.
-          if(year == 2016 && month == 1) {
-            console.log("lastday" + lastDay);
-          }
+          let lastDay = new Date(year, month + 1, 0).getDate();
           let dayArray = new Array();
-          for(let k = 0; k < lastDay; k ++) {
+          for (let k = 0; k < lastDay; k++) {
             let day = new Object();
-            day.value = k + 1 + "日";
+            day.value = k + 1;
             day.text = k + 1 + "日";
             dayArray.push(day);
           }
           let monthObj = new Object();
           monthObj.children = dayArray;
-          monthObj.value = j + 1 + "月";
+          monthObj.value = j + 1;
           monthObj.text = j + 1 + "月";
           monthArray.push(monthObj);
         }
         let yearObj = new Object();
-        yearObj.value = i + 2016 + "年" ;
+        yearObj.value = i + 2016;
         yearObj.text = i + 2016 + "年";
         yearObj.children = monthArray;
         yearArray.push(yearObj);
-      };
+      }
+      ;
       picker.setData(yearArray);
 
-      let showUserPickerButton3 = document.getElementById('showCityPicker3');
-      let showUserPickerButton = document.getElementById('showCityPicker');
-      let userResult3 = document.getElementById('cityResult3');
-      let userResult = document.getElementById('cityResult');
-      showUserPickerButton.addEventListener('tap', function(event) {
-        picker.show(function(items) {
-          userResult.innerText =  "" + _getParam(items[0], 'text') + " " + _getParam(items[1], 'text') + " " + _getParam(items[2], 'text');
-          // userResult.innerText = JSON.stringify(items[0]);
-          //返回 false 可以阻止选择框的关闭
+      let startBtn = document.getElementById('startDate');
+      let endBtn = document.getElementById('endDate');
+      let userResult3 = document.getElementById('startDateBtn');
+      let userResult = document.getElementById('endDateBtn');
+      endBtn.addEventListener('tap', function (event) {
+        picker.show(function (items) {
+          userResult.innerText = "" + _getParam(items[0], 'text') + " " + _getParam(items[1], 'text') + " " + _getParam(items[2], 'text');
+          console.log(items)
           //return false;
         });
       }, false);
-      showUserPickerButton3.addEventListener('tap', function(event) {
-        picker.show(function(items) {
-          userResult3.innerText =  "" + _getParam(items[0], 'text') + " " + _getParam(items[1], 'text') + " " + _getParam(items[2], 'text');
-          // userResult.innerText = JSON.stringify(items[0]);
-          //返回 false 可以阻止选择框的关闭
+      startBtn.addEventListener('tap', function (event) {
+        picker.show(function (items) {
+          userResult3.innerText = "" + _getParam(items[0], 'text') + " " + _getParam(items[1], 'text') + " " + _getParam(items[2], 'text');
+          console.log(items)
           //return false;
         });
       }, false);
+
     }
   }
 </script>
 
-<style scoped>
-  /*@import 'vue-awesome-mui/mui/plugin/picker/css/mui.picker.css'*/
-  /*@import 'vue-awesome-mui/mui/plugin/picker/css/mui.poppicker.css'*/
+<style scoped lang="less">
   .mui-btn {
     .mui-btn-block {
       background-color: #DDAF59;
