db8fec2661b4bdf77def336929fcb843b40a7817
dashboard小队 <879089262@qq.com>
2019-08-06 12:44:00 +0800

    +++

---
+++ src/router/hujin.js
@@ -0,0 +1,8 @@
+let routes = [
+  {
+    path: '/hujin',
+    name: 'hujin',
+    component: resolve => require(['@/report/hujin'], resolve)
+  }]
+
+export default routes;
--- src/router/index.js
+++ src/router/index.js
@@ -7,21 +7,12 @@ const baseRoutes = [
     path: '/',
     name: 'index',
     component: resolve => require(['@/home/index'], resolve)
-  },
-  {
-    path: '/hujin',
-    name: 'hujin',
-    component: resolve => require(['@/report/hujin'], resolve)
-  },
-  {
-    path: '/test',
-    name: 'test',
-    component: resolve => require(['@/report/test'], resolve)
   }
 ];
 import ydRoutes  from '@/router/yd';
 import yjyRoutes from '@/router/yjy'
-const routes = baseRoutes.concat(ydRoutes,yjyRoutes);
+import hujinRoutes from '@/router/hujin'
+const routes = baseRoutes.concat(ydRoutes,yjyRoutes,hujinRoutes);
 
 export default new Router({
   base: 'dashboard-report',
