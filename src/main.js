998ea32635eb84c614dd14810f57af4d05539aa8
dashboard小队 <879089262@qq.com>
2019-08-14 14:50:06 +0800

    +++

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ Vue.prototype.$http = axios
 import echarts from 'echarts'
 Vue.prototype.$echarts = echarts
 
-import 'vx-easyui/dist/themes/default/easyui.css'
+import 'vx-easyui/dist/themes/myTheme/easyui.css'
 import 'vx-easyui/dist/themes/icon.css'
 import 'vx-easyui/dist/themes/vue.css'
 import '@/assets/css/easyui/myEasyui.css'
