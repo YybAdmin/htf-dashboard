40c6e77d16b4a39f08fa849c59f0b7df61b0ad62
dashboard小队 <879089262@qq.com>
2019-08-14 11:55:13 +0800

    +++

--- src/components/baseCom/rankAllList.vue
+++ src/components/baseCom/rankAllList.vue
@@ -55,10 +55,12 @@
         SwiperOptions: {
           freeMode:true,
           slidesPerView: 3,
-          spaceBetween: -4,
-          centeredSlides : true
+          spaceBetween: -6,
+          centeredSlides : true,
+          initialSlide:1,
+          loop: false
         },
-        itemList: []
+        itemList: [{NAME:'-',VALUE:'-'},{NAME:'-',VALUE:'-'},{NAME:'-',VALUE:'-'}]
       }
     },
     computed: {
